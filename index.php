b87b1b2799ac79f85f7d1cfeeeb3b0a2ae92d7c7
Yoonus Tehrani <yoonustehrani28@gmail.com>
2019-01-05 18:52:36 +0330

    Just added a loop to code

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php 
-echo "hello world";
+// echo "hello world";
+// You must have a loop
+for ($i=0; $i < 3; $i++) {
+	switch ($i) {
+		case 1:
+			$numb = "first";
+		break;
+		case 2:
+			$numb = "sceond";
+		default:
+			$num = "third";
+		break;
+	} 
+	echo "hello for the ".$numb." time";
+}
